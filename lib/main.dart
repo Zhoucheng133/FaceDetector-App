2083409af48cc68e4b6dcff275d3228ddec09d6b
zhoucheng <zhoucheng133@outlook.com>
2026-07-02 21:11:35 +0800

    完成窗口配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,83 @@
+import 'package:face_detector_app/getx/controller.dart';
+import 'package:face_detector_app/lang/zh_cn.dart';
+import 'package:face_detector_app/main_window.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  final Controller controller = Get.put(Controller());
+  await controller.init();
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+    title: "FaceDetector"
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class MainTranslations extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    // 'en_US': enUS,
+    'zh_CN': zhCN,
+    // 'zh_TW': zhTW,
+  };
+}
+
+
+class _MainAppState extends State<MainApp> {
+
+  final Controller controller = Get.find();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return GetMaterialApp(
+      translations: MainTranslations(), 
+      debugShowCheckedModeBanner: false,
+      locale: controller.lang.value.locale, 
+      fallbackLocale: supportedLocales[0].locale,
+      supportedLocales: supportedLocales.map((item)=>item.locale).toList(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      theme: ThemeData(
+        brightness: Theme.of(context).brightness,
+        fontFamily: 'PuHui', 
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+          brightness: Theme.of(context).brightness,
         ),
+        textTheme: Theme.of(context).brightness==Brightness.dark ? ThemeData.dark().textTheme.apply(
+          fontFamily: 'PuHui',
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        ) : ThemeData.light().textTheme.apply(
+          fontFamily: 'PuHui',
+        ),
+      ),
+      home: Scaffold(
+        body: MainWindow()
       ),
     );
   }
