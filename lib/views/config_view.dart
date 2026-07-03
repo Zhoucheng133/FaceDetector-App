523749fac86f16af9a5a1b2c70831b2c61316e30
zhoucheng <zhoucheng133@outlook.com>
2026-07-03 19:29:08 +0800

    添加配置页布局

--- lib/views/config_view.dart
+++ lib/views/config_view.dart
@@ -10,6 +10,23 @@ class ConfigView extends StatefulWidget {
 class _ConfigViewState extends State<ConfigView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        SizedBox(
+          width: 200,
+        ),
+        Expanded(
+          child: Padding(
+            padding: .only(left: 15, bottom: 15, right: 15, top: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
